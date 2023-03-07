a4f4b3b110404eaa71fcdd86269e10f25399cc0b
Sinclair-boop <ngapoutfotso@gmail.com>
2023-03-06 17:04:13 -0800

    first commit

---
+++ src/views/QuizeView.vue
@@ -0,0 +1,48 @@
+<script setup>
+import Question from "../components/Question.vue"
+import QuizeHeader from "../components/QuizHeader.vue"
+import Result from "../components/Result.vue"
+import {useRoute} from "vue-router"
+import {ref, watch, computed} from "vue"
+import quizes from "../data/quizes.json"
+const route = useRoute()
+const quizId = parseInt(route.params.id)
+const quiz = quizes.find(q => q.id === quizId)
+const currentQuestionIndex = ref(0)
+const numberOfCorrectAnswers = ref(0)
+const showResults = ref(false)
+const onOptionSelected = (isCorrect) => {
+    if (isCorrect){
+    numberOfCorrectAnswers.value++
+    }
+    if(quiz.questions.length - 1 === currentQuestionIndex.value){
+        showResults.value = true
+    }
+    currentQuestionIndex.value++
+}
+// const questionStatus = ref(`${currentQuestionIndex.value + 1}/${quiz.questions.length}`)
+// watch(() => currentQuestionIndex.value, () => {
+//     questionStatus.value = `${currentQuestionIndex.value + 1}/${quiz.questions.length}`
+// })
+const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+const barPercentage = computed(() => `${currentQuestionIndex.value/quiz.questions.length*100}%`)
+</script>
+<template>
+    <div>
+        <QuizeHeader :questionStatus = "questionStatus" :barPercentage = "barPercentage"/>
+        <div>
+            <Question 
+                v-if="!showResults"
+                :question = "quiz.questions[currentQuestionIndex]" 
+                @selectOption="onOptionSelected"
+            />
+            <Result
+                v-else
+                :quizQuestionLength  = "quiz.questions.length"
+                :numberOfCorrectAnswers = "numberOfCorrectAnswers"
+            />
+            <!-- {{ numberOfCorrectAnswers }} -->
+        </div>
+        <!-- <button @click = "currentQuestionIndex++">Next Question</button> -->
+    </div>
+</template>
